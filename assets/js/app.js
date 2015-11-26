269c59a6d10683069434b6dcb4532443fd6557ab
pelo88 <pelo8888@gmail.com>
2015-11-25 23:13:59 -0300

    Levanto el carrito desde las cookies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,61 @@
-var app = angular.module('pelo', []);
+(function() {
+  var app = angular.module('taller', ['ngCookies']);
 
-app.controller('RecomendedController', function($scope, $http) {
-  $scope.recomendados = [];
+  app.controller('RecomendedController', function($scope, $http, $cookies) {
+    $scope.recomendados = [];
 
-  $http.get('json/recomendados.json')
-    .success(function(data) {
-      $scope.recomendados = data;
-    })
-    .error(function(res) {
-      console.error('Error:' + res);
-    });
+    $http
+      .get('json/recomendados.json')
+      .success(function(data) {
+        $scope.recomendados = data;
+      })
+      .error(function(res) {
+        console.error('Error:' + res);
+      });
 
-  $scope.addToCart = function(model) {
+    $scope.addToCart = function(model) {
 
-    if (!Revenga.carrito.find(function(e) {
-      if (e.id === model.id) {
-        e.count++;
-        return true;
+      Revenga.carrito = Revenga.carrito || [];
+
+      if (!Revenga.carrito.find(function(e) {
+        if (e.id === model.id) {
+          e.count++;
+          return true;
+        }
+      })) {
+        Revenga.carrito.push(model);
       }
-    })) {
-      Revenga.carrito.push(model);
+
+      $cookies.putObject('carrito', Revenga.carrito);
+
+      toastr.options.positionClass = "toast-bottom-right";
+      toastr.success('Este producto ha sido agregado a tu carrito de compras.', 'Carrito actualizado');
+    };
+
+  });
+
+
+  app.controller('QuickCartController', function($scope, $cookies) {
+
+    if (!Revenga.carrito) {
+      Revenga.carrito = $cookies.getObject('carrito');
     }
 
-    toastr.options.positionClass = "toast-bottom-right";
-    toastr.success('Este producto ha sido agregado a tu carrito de compras.', 'Carrito actualizado');
-  };
+    $scope.carrito = Revenga.carrito || [];
 
-});
+    $scope.getTotal = function() {
+      var total = 0;
+      for (var i = 0; i < $scope.carrito.length; i++) {
+        var product = $scope.carrito[i];
+        total += (product.price * product.count);
+      }
+      return total;
+    };
 
-
-app.controller('QuickCartController', function($scope, $http) {
-  $scope.carrito = Revenga.carrito;
-
-  $scope.getTotal = function() {
-    var total = 0;
-    for (var i = 0; i < $scope.carrito.length; i++) {
-      var product = $scope.carrito[i];
-      total += (product.price * product.count);
+    $scope.getLength = function() {
+      return $scope.carrito.length;
     }
-    return total;
-  };
 
-  $scope.getLength = function() {
-    return $scope.carrito.length;
-  }
-});
+  });
+
+})();
--- assets/js/revenga.js
+++ assets/js/revenga.js
@@ -1,9 +1,29 @@
 var Revenga = Revenga || {};
 
 (function(fn) {
-  fn.carrito = [];
+  fn.carrito = undefined;
   fn.productos = [];
 
+  fn.paintMap = function() {
+    contactMap = new GMaps({
+      div: '#contactMap',
+      lat: -34.8357249,
+      lng: -56.1918285
+    });
 
+    contactMap.addMarker({
+      lat: -34.8357249,
+      lng: -56.1918285,
+      title: 'Taller Revenga',
+      click: function(e) {
+        alert('Revenga Ltda.\n' +
+          'Av de las Instrucciones 1489\n' +
+          'Montevideo, Uruguay\n' +
+          'Tel: (+598) 2354 2004\n' +
+          'Cel: 099 620 522\n' +
+          'info@revenga.com.uy\n');
+      }
+    });
+  }
 
-})(Revenga)
+})(Revenga);
